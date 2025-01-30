d10cd873658fa6b05182a3327a7326914c366867
Arthur <artrampnau@mail.ru>
2025-01-30 18:18:42 +0300

    Initial Commit

---
+++ Flippy bird cloneTests/Flippy_bird_cloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  Flippy_bird_cloneTests.swift
+//  Flippy bird cloneTests
+//
+//  Created by Arthur Trampnau on 30/01/25.
+//
+
+import Testing
+@testable import Flippy_bird_clone
+
+struct Flippy_bird_cloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Flippy bird cloneUITests/Flippy_bird_cloneUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Flippy_bird_cloneUITestsLaunchTests.swift
+//  Flippy bird cloneUITests
+//
+//  Created by Arthur Trampnau on 30/01/25.
+//
+
+import XCTest
+
+final class Flippy_bird_cloneUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
